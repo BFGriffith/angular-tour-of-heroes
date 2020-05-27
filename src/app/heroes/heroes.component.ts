67dba7355a15b91d26383783b0b2edf7f9ba5cf7
Ben Griffith <bengriffith@deloitte.com>
2020-05-27 15:24:56 -0400

    toh Part1 Single Hero Editor

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -3,7 +3,6 @@
 //=> src/app/heroes/heroes.component...
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
-import { HEROES } from '../mock-heroes';
 
 @Component({
   selector: 'app-heroes',
@@ -11,14 +10,15 @@ import { HEROES } from '../mock-heroes';
   styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent implements OnInit {
-  heroes = HEROES;
-  selectedHero: Hero;
-
+  /*
+  hero = 'Windstorm';
+  */
+  hero: Hero = {
+    id: 1,
+    name: 'Windstorm'
+  };
   constructor() { }
   ngOnInit() {
-  }
-  onSelect(hero: Hero): void {
-    this.selectedHero = hero;
   }
 }
 /*
